b6714348f081e24f54a583a945c9ffdf3a0a05a3
Arthur Papailhau <papailhau@gmail.com>
2016-02-28 22:04:34 +0100

    Add architecture SideMenu

--- AmicaleINSA/ViewController.swift
+++ AmicaleINSA/ViewController.swift
@@ -7,17 +7,23 @@
 //
 
 import UIKit
+import SWRevealViewController
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment this line to define a global platform for your project
+ platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+ use_frameworks!
+
+target 'AmicaleINSA' do
+    pod 'SWRevealViewController', :git => 'https://github.com/John-Lluch/SWRevealViewController.git'
+end
+
+target 'AmicaleINSATests' do
+
+end
+
+target 'AmicaleINSAUITests' do
+
+end
+
